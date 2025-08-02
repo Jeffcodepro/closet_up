e68572c8b05ce182dcf29d1a3e53c0f26f6c576f
Jefferson Santos <jeffersonoliveira1212@gmail.com>
2025-08-02 19:50:09 -0300

    Configuração da gem devise e todos os models

---
+++ db/migrate/20250802224709_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.text :description
+      t.string :sku
+      t.decimal :price
+      t.references :category, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
