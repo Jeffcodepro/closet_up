dc4e0d856303ce648d88d6d833caddbd2158b4b9
Jefferson Santos <jeffersonoliveira1212@gmail.com>
2025-08-07 19:30:08 -0300

    Um pouco de front e rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'offers/index'
-  get 'offers/show'
-  get 'offers/new'
-  get 'offers/create'
-  get 'products/index'
-  get 'products/show'
   devise_for :users
   root to: "products#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -24,17 +18,12 @@ Rails.application.routes.draw do
 
   get "/products/:id", to: "products#show", as: :product
 
-  # Filtro por categoria
   get '/products/category/:category_id', to: 'products#index', as: :products_by_category
 
-  # Criar nova oferta para um produto específico
   get  '/products/:product_id/offers/new', to: 'offers#new', as: :new_product_offer
   post '/products/:product_id/offers',     to: 'offers#create', as: :product_offers
 
-  # Listar todas as ofertas
   get '/offers', to: 'offers#index', as: :offers
 
-  # Ver uma oferta específica
   get '/offers/:id', to: 'offers#show', as: :offer
-
 end
